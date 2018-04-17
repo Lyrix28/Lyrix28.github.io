416f790f30c7dc2c10dc609d0d00866cf76612f6
Lyrix28 <Lyrix28@users.noreply.github.com>
2018-04-17 22:43:42 +0800

    0317
    hw4

---
+++ assets/UnityProject/hw4/Assets/MyComponent/Scripts/Disk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disk : MonoBehaviour {
+
+	public float speed;
+	public Vector3 size;
+	public Vector3 position;
+	public Vector3 direction;
+	public Color color;
+
+}
---
+++ assets/UnityProject/hw4/Assets/MyComponent/Scripts/MyEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Disk))]
+public class MyEditor : Editor
+{ 
+	public override void OnInspectorGUI()
+	{
+		var target = (Disk)(serializedObject.targetObject);
+		target.speed = EditorGUILayout.FloatField("SPEED",target.speed);
+		target.size = EditorGUILayout.Vector3Field ("SIZE",target.size);
+		target.position = EditorGUILayout.Vector3Field ("POSITION",target.position);
+		target.direction = EditorGUILayout.Vector3Field ("DIRECTION",target.direction);
+		target.color = EditorGUILayout.ColorField("COLOR",target.color);
+
+	}
+
+}
